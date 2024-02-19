4057dc715d2428a6058cc8492aec71baafc23eb1
andresinh0 <157610208+andresinh0@users.noreply.github.com>
2024-02-19 18:28:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.getElementById('togglePassword').addEventListener('click', function() {
     var passwordInput = document.getElementById('password');
     var icon = document.getElementById('togglePassword');
@@ -9,4 +8,9 @@ document.getElementById('togglePassword').addEventListener('click', function() {
         passwordInput.type = "password";
         icon.setAttribute('name', 'lock-closed-outline');
     }
+});
+
+window.addEventListener('load', function() {
+    const loadingImage = document.querySelector('.loading-image');
+    loadingImage.style.display = 'none'; // Oculta la imagen de carga una vez que la página se haya cargado completamente
 });
