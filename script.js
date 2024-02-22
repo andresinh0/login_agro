358547c87dc726c8a4a922c2ea9edc85b0f3a456
andresinh0 <157610208+andresinh0@users.noreply.github.com>
2024-02-22 17:24:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('togglePassword').addEventListener('click', function() {
+document.getElementById('togglePassword').addEventListener('click', function() { /* esto es para cambiar el logo de ver la clave */
     var passwordInput = document.getElementById('password');
     var icon = document.getElementById('togglePassword');
     if (passwordInput.type === "password") {
@@ -10,19 +10,48 @@ document.getElementById('togglePassword').addEventListener('click', function() {
     }
 });
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function() { /* esto es para el loading */
     const loadingImage = document.querySelector('.loading-image');
     loadingImage.style.display = 'none'; // Oculta la imagen de carga una vez que la página se haya cargado completamente
 });
 
 
-document.getElementById('toggleDarkMode').addEventListener('click', function() {
+document.getElementById('toggleDarkMode').addEventListener('click', function() { /* esto es para el dark mode */
     document.body.classList.toggle('dark-mode'); // Alternar la clase dark-mode en el cuerpo del documento al hacer clic en el botón
     
     var icon = document.getElementById('toggleDarkMode').querySelector('ion-icon');
     if (document.body.classList.contains('dark-mode')) {
-        icon.setAttribute('name', 'moon-outline'); // Cambiar el icono a modo oscuro
+        icon.setAttribute('name', 'moon-outline');
     } else {
-        icon.setAttribute('name', 'sunny-outline'); // Cambiar el icono a modo claro
+        icon.setAttribute('name', 'sunny-outline');
     }
 });
+
+// esto es para inicializar el reproductor de video de YT
+
+// var player;
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('video-container', {
+//         height: '100%',
+//         width: '100%',
+//         videoId: 'T-YxHUoIEAU',
+//         playerVars: {
+//             autoplay: 1,
+//             controls: 0,
+//             showinfo: 0,
+//             modestbranding: 1,
+//             loop: 1,
+//             playlist: 'T-YxHUoIEAU'
+//         },
+//         events: {
+//             'onReady': onPlayerReady
+//         }
+//     });
+// }
+
+// para un video de YT de fondo
+// function onPlayerReady(event) {
+//     event.target.mute();
+//     event.target.setPlaybackQuality('hd1080');
+//     event.target.playVideo();
+// }
